8f449303d0d4eb2e4664b3c516b1f2790a9e1cf1
Mohammed Elnawawy <shekoelnawawy@gmail.com>
2024-06-08 16:25:05 +0400

    update 83

--- main.py
+++ main.py
@@ -5,37 +5,67 @@ if os.path.exists('./output'):
 
 os.system('mkdir ./output')
 
+print('Training:')
+os.system('python RGAN.py --settings_file ohiot1dm > ./output/train_all.txt')
+
 # with is like your try .. finally block in this case
-with open('./experiments/settings/ohiot1dm.txt', 'r') as train_file:
+with open('./experiments/settings/ohiot1dm_test.txt', 'r') as test_file:
     # read a list of lines into data
-    train_data = train_file.readlines()
+    test_data = test_file.readlines()
 
-for run in range(10):
-    # now change the 2nd line, note that you have to add a newline
-    train_data[3] = "\"patient\": \"" + str(run) + "\",\n"
+print('Testing: ')
+for year in [2020, 2018]:
+    test_data[2] = "\"year\": \"" + str(year) + "\",\n"
 
-    # and write everything back
-    with open('./experiments/settings/ohiot1dm.txt', 'w') as train_file:
-        train_file.writelines(train_data)
+    for patient in range(6):
+        test_data[3] = "\"patient\": \"" + str(patient) + "\",\n"
 
-    print('Training: '+str(run))
-    os.system('python RGAN.py --settings_file ohiot1dm > ./output/train_'+ str(run) + '.txt')
+        # and write everything back
+        with open('./experiments/settings/ohiot1dm_test.txt', 'w') as test_file:
+            test_file.writelines(test_data)
 
-    # with is like your try .. finally block in this case
-    with open('./experiments/settings/ohiot1dm_test.txt', 'r') as test_file:
-        # read a list of lines into data
-        test_data = test_file.readlines()
+        print('Year: '+str(year)+'\tPatient: '+str(patient))
+        os.system('python AD.py --settings_file ohiot1dm_test > ./output/test_all_patient_' + str(year) + '_' + str(patient) +'.txt')
 
-    print('Testing: ')
-    for year in [2020, 2018]:
-        test_data[2] = "\"year\": \"" + str(year) + "\",\n"
 
-        for patient in range(6):
-            test_data[3] = "\"patient\": \"" + str(patient) + "\",\n"
-
-            # and write everything back
-            with open('./experiments/settings/ohiot1dm_test.txt', 'w') as test_file:
-                test_file.writelines(test_data)
-
-            print('Year: '+str(year)+'\tPatient: '+str(patient))
-            os.system('python AD.py --settings_file ohiot1dm_test > ./output/test_run_' + str(run) + '_patient_' + str(year) + '_' + str(patient) +'.txt')
+# import os
+#
+# if os.path.exists('./output'):
+#     os.system('"yes" yes | rm -r ./output')
+#
+# os.system('mkdir ./output')
+#
+# # with is like your try .. finally block in this case
+# with open('./experiments/settings/ohiot1dm.txt', 'r') as train_file:
+#     # read a list of lines into data
+#     train_data = train_file.readlines()
+#
+# for run in range(10):
+#     # now change the 2nd line, note that you have to add a newline
+#     train_data[3] = "\"patient\": \"" + str(run) + "\",\n"
+#
+#     # and write everything back
+#     with open('./experiments/settings/ohiot1dm.txt', 'w') as train_file:
+#         train_file.writelines(train_data)
+#
+#     print('Training: '+str(run))
+#     os.system('python RGAN.py --settings_file ohiot1dm > ./output/train_'+ str(run) + '.txt')
+#
+#     # with is like your try .. finally block in this case
+#     with open('./experiments/settings/ohiot1dm_test.txt', 'r') as test_file:
+#         # read a list of lines into data
+#         test_data = test_file.readlines()
+#
+#     print('Testing: ')
+#     for year in [2020, 2018]:
+#         test_data[2] = "\"year\": \"" + str(year) + "\",\n"
+#
+#         for patient in range(6):
+#             test_data[3] = "\"patient\": \"" + str(patient) + "\",\n"
+#
+#             # and write everything back
+#             with open('./experiments/settings/ohiot1dm_test.txt', 'w') as test_file:
+#                 test_file.writelines(test_data)
+#
+#             print('Year: '+str(year)+'\tPatient: '+str(patient))
+#             os.system('python AD.py --settings_file ohiot1dm_test > ./output/test_run_' + str(run) + '_patient_' + str(year) + '_' + str(patient) +'.txt')
